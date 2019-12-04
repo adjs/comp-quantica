3a420ba60f2a04bf3a430d92411ed29598e4751c
mayrtondias <mayrtondias@users.noreply.github.com>
2019-12-04 19:48:12 -0300

    Grupo 04  (#16)
    * Apresentação Inicial Grupo 04
    
    Grupo: Claudio, Gustavo e Mayrton
    
    * notebook
    
    * update grupo 4 circuit
    
    * first commit - gisf
    
    * Merge com minhas mudanças
    
    * Deletando arquivo antigo
    
    * Excluindo a apresentação em pdf
    
    Apagando o arquivo da apresentação em pdf
    
    * Ajuste imagem
    
    * Adicionando imagem
    
    * init z-pauli automated
    
    * Complemento grafo/hipergrafo e estados REW
    
    * ajuste conflitos
    
    * Ajuste na definição de grafos
    
    * Tentativa de criar Uw, toffoli e 3/4-z-controlado
    
    * Toffoli okay, n-z-controlado feito separadamente em cccz gate
    
    * n-z-controlado implementado em notebook principal; circuito quantico concluido
    
    * Merge branch 'master' into gustavoBranch
    
    * Circuito quantico para hipergrafo completo, imagens corrigidas
    
    * Corrigindo imagem 7
    
    * Circuito corrigido e finalizado
    
    * 3-toffoli corrigida pra 4-toffoli
    
    * Transformacao da imagem em vetor
    
    * 'update'
    
    * sign flip algorithm automation
    
    * 'upload'
    
    * 'vai'
    
    * Corrigindo exclusões
    
    * codigo hipergrafos - start
    
    * Hipergrafos: A saga continua
    
    * Hipergrafos: Ui e Uw funcionando
    
    * Hipergrafos: Concluido! (:
    
    * Simplificacao com o Algoritmo QuineMcCluskey
    
    * Ajuste vetor de entrada
    
    * Ajuste simbolos
    
    * Comparacao dos algoritmos

---
+++ projetos/Grupo 04/pqm.py
@@ -0,0 +1,95 @@
+'''
+PQM class for probabilistic quantum memories
+'''
+
+# pylint: disable=no-member
+
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, BasicAer, execute
+import sympy as sp
+
+
+class PQM:
+    ''' Simulation of a probabilistic quantum memory based
+        on arXiv:quant-ph/0204115
+    '''
+
+    def __init__(self, size):
+        '''
+            Creates a probabilistic quantum memory
+            size: Number of qbits used in the memory
+        '''
+        self._memory = QuantumRegister(size, 'm')
+        self._ancillary = QuantumRegister(1, 'c')
+        self._bit = ClassicalRegister(1, 'bit')
+        self._size = size
+        self._circuit = QuantumCircuit(self._memory, self._ancillary, self._bit)
+
+    def load_data(self, data=None):
+        ''' memory initialization'''
+
+        params = (2**self._size) * [0]
+
+        if data is None:
+            params[0] = 1
+        else:
+            number_patterns = len(data)
+            amplitude = 1.0 / sp.sqrt(number_patterns)
+
+            for pattern in data:
+                index = self._bin_pattern_to_number(pattern)
+                params[index] = amplitude
+
+        self._circuit.initialize(params, self._memory)
+
+    def recover(self, input_pattern=None):
+        '''
+        :param input_pattern: (size,) array of 0s and 1s
+                              input pattern for recovering algorithm
+        :return circuit:      quantum circuit of PQM recovering algorithm
+        '''
+
+        if input_pattern is not None:
+            for k, value in enumerate(input_pattern):
+                if value == 1:
+                    self._circuit.x(self._memory[k])
+        for k in range(self._size):
+            self._circuit.u1(sp.pi / 2 * self._size, self._memory[k])
+
+        # initialize auxiliary quantum bit |c>
+        self._circuit.h(self._ancillary[0])
+        for k in range(self._size):
+            self._circuit.cu1(- sp.pi / self._size, self._ancillary[0], self._memory[k])
+        self._circuit.h(self._ancillary[0])
+
+        self._circuit.measure(self._ancillary[0], self._bit[0])
+
+    def run(self, number_shots=1024, backend=BasicAer.get_backend('qasm_simulator')):
+        '''
+        :param backend: Backend to run the PQM
+        :param number_shots: Number of repetitions
+        :return: number of 0s and 1s after number_shots executions
+        '''
+        job = execute(self._circuit, backend, shots=number_shots)
+        result = job.result()
+        counts = result.get_counts(self._circuit)
+        return counts
+
+    @staticmethod
+    def _bin_pattern_to_number(pattern):
+        aindex = 0
+        for i, value in enumerate(pattern):
+            aindex += value * 2 ** i
+        return aindex
+
+    def __str__(self):
+        str_circuit = self._circuit.draw()
+        return str(str_circuit)
+
+if __name__ == '__main__':
+    pqm = PQM(2)
+    print(pqm.__dict__)
+    pqm.load_data([[0, 0], [0, 1]])
+    pqm.recover()
+    out = pqm.run(8192)
+    print(str(out['0']/8192) + '%')
+    print(pqm)
