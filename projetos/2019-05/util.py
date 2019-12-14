fabdc94987927288a3671962c945aadf4638a469
Tiago de Lima <33109618+Tiagoblima@users.noreply.github.com>
2019-12-14 16:23:40 -0300

    Correções no artigo

--- projetos/2019-05/util.py
+++ projetos/2019-05/util.py
@@ -5,8 +5,8 @@ from qiskit.providers.jobstatus import JOB_FINAL_STATES
 import numpy as np
 import time
 
-def get_possible_inputs(size, positions=None):
 
+def get_possible_inputs(size, positions=None):
     if positions is None:
         all_positions = generate_positions(size)
 
@@ -17,14 +17,15 @@ def get_possible_inputs(size, positions=None):
     """
     array = []
     binary = [0] * size
-    
+
     for tup in all_positions:
         for index in tup:
             binary[index] = 1
-        binary = [0]*size
+        binary = [0] * size
         array.append(binary)
 
     return array
+
 
 def generate_positions(inputs_size):
     """Generate the inputs to the QBNN combining the positions of
@@ -33,10 +34,10 @@ def generate_positions(inputs_size):
     inputs = []
     regs = np.arange(inputs_size)
     for n_reg in range(inputs_size + 1):
-        
         inputs.extend(list(combinations(regs, n_reg)))
-    
+
     return inputs
+
 
 def get_results(circ, output, backend=None, shots=1024, circ_name='CIRCUIT'):
     """Shows the results given by the _circuit"""
@@ -57,11 +58,10 @@ def get_results(circ, output, backend=None, shots=1024, circ_name='CIRCUIT'):
     count = result.get_counts(circ)
 
     qub_chance = []
-   
-   
+
     for qub, cou in zip(count.keys(), count.values()):
         chance = (cou / shots) * 100
-    
+
         print("|{}> -> {:.2f}%".format(qub, chance), end='\t')
 
         count[qub] = '{:.2f}%'.format(chance)
